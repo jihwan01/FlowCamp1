eaf0d3cff881e8a5672ce33fbdc1f90051d674ee
kyeah <dmobtxx19@gmail.com>
2023-06-30 23:30:47 +0900

    일단 돌아가는 버전

--- app/src/main/java/com/example/flowcamp1/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/flowcamp1/ui/dashboard/DashboardFragment.java
@@ -1,37 +1,46 @@
 package com.example.flowcamp1.ui.dashboard;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.session.PlaybackState;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.flowcamp1.MainActivity;
+import com.example.flowcamp1.R;
 import com.example.flowcamp1.databinding.FragmentDashboardBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DashboardFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    public GridView mGridView;
+    public DashboardAdapter mAdapter;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        MainActivity activity = (MainActivity) getActivity();
+        Context context = container.getContext();
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_dashboard, container, false);
+        mGridView = rootView.findViewById(R.id.gallery);
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
+        List<DashboardItem> itemList = new ArrayList<>();
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.gallery);
+        itemList.add(new DashboardItem(bitmap));
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+        mAdapter = new DashboardAdapter(context, itemList);
+        mGridView.setAdapter(mAdapter);
+
+        return rootView;
     }
 }
