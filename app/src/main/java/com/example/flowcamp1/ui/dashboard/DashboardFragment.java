c6b0d0fc0cf0d3c7e95221856af2a20a6908bb0d
kyeah <dmobtxx19@gmail.com>
2023-07-01 18:57:27 +0900

    갤러리 입장

--- app/src/main/java/com/example/flowcamp1/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/flowcamp1/ui/dashboard/DashboardFragment.java
@@ -1,18 +1,33 @@
 package com.example.flowcamp1.ui.dashboard;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.session.PlaybackState;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -28,6 +43,28 @@ public class DashboardFragment extends Fragment {
 
     public GridView mGridView;
     public DashboardAdapter mAdapter;
+
+    private void getGalleryPermission(Activity activity, Context context) {
+        String temp = "";
+        int permissionForGalleryRead = ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (permissionForGalleryRead != PackageManager.PERMISSION_GRANTED)
+            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
+
+        int permissionForGalleryWrite = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionForGalleryWrite != PackageManager.PERMISSION_GRANTED)
+            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
+        if (!temp.isEmpty())
+            ActivityCompat.requestPermissions(activity, temp.trim().split(" "), 1);
+    }
+
+    private void getCameraPermission(Activity activity, Context context) {
+        String temp = "";
+        int permissionForCamera = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA);
+        if (permissionForCamera != PackageManager.PERMISSION_GRANTED)
+            temp = Manifest.permission.CAMERA;
+        if (!temp.isEmpty())
+            ActivityCompat.requestPermissions(activity, temp.trim().split(" "), 1);
+    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         MainActivity activity = (MainActivity) getActivity();
@@ -50,6 +87,35 @@ public class DashboardFragment extends Fragment {
         mAdapter = new DashboardAdapter(context, itemList);
         mGridView.setAdapter(mAdapter);
 
+        setHasOptionsMenu(true);
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_gallery, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == R.id.gallery_add){
+            Activity activity = getActivity();
+            Context context = getContext();
+            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+            Log.v("TEST", builder+".");
+            builder.setMessage("사진 추가하기");
+
+            builder.setPositiveButton("갤러리에서 가져오기", (dialog, which) ->{
+                getGalleryPermission(activity, context);
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 101);
+            });
+
+            builder.create().show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
